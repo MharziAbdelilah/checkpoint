e8a80f1e39ac565a976a44054fce832cf0030be5
MharziAbdelilah <mharzi.abdelilah@gmail.com>
2024-05-29 19:15:03 +0100

    checkpoint

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Card, Col, Row } from 'react-bootstrap';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+          <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#">GomyCode</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Container  className="mt-4"  >
+          <h1 className='text-center'>Hello, GomyCode</h1>
+
+          <Row>
+            <Col> 
+            <Card className="card text-start">
+              <div className="card-body">
+                <h4 className="card-title">search for navbars</h4>
+                <p className="card-text">Body</p>
+              </div>
+            </Card>
+            </Col>
+
+            <Col> 
+            <Card className="card text-start">
+              <div className="card-body">
+                <h4 className="card-title">Title</h4>
+                <p className="card-text">Body</p>
+              </div>
+            </Card>
+            </Col>
+
+            <Col> 
+            <Card className="card text-start">
+              <div className="card-body">
+                <h4 className="card-title">Title</h4>
+                <p className="card-text">Body</p>
+              </div>
+            </Card>
+            </Col>
+          </Row>
+        </Container>
+    </>
   );
 }
 
